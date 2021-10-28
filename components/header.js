3ff5db99fc903378de51bf6c59c97ce7161ed6c8
OpeDada <opedada@gmail.com>
2021-10-28 13:23:39 +0900

    inserted bootstrap header style

--- components/header.js
+++ components/header.js
@@ -1,34 +1,33 @@
 import React, { Component } from 'react'
 import Link from 'next/link'
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
 export default class Header extends Component {
   render() {
     return (
-      <div>
-        <header class="header-group">
-        <div>
-        <h2>Beadaut Blogs</h2>
-        <p>Curated posts and updates from Beadaut</p>
-        </div>
-        <ul>
-          <li>
-            <Link href={"/posts-list/posts"}>
-              <a>Blogs</a>
-            </Link>
-          </li>
-          <li>
-            <Link href={"https://www.beadaut.com/"}>
-              <a className="foo" target="_blank">About</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/">
-              <a className="foo" target="_blank">Updates</a>
-            </Link>
-          </li>
-        </ul>
-      </header>
-      </div>
+      <div className="header-group">
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand href="#home">Beadaut Blogs</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="">
+                <Link href={"/posts-list/posts"}>
+                  <a className="nav-link">Blogs</a>
+                </Link>
+                <Link href={"https://www.beadaut.com/"}>
+                  <a className="nav-link" target="_blank">About</a>
+                </Link>
+                <Link href="/">
+                  <a className="nav-link" target="_blank">Updates</a>
+                </Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+    </div>
     )
   }
 }
