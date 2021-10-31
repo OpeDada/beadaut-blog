7d9bde0b3c78dde40a99ffab88f5db0e1767b975
Joshua Owoyemi <tjosh.owoyemi@gmail.com>
2021-10-31 22:48:44 +0900

    Apply formating to header.js

--- components/header.js
+++ components/header.js
@@ -1,34 +1,51 @@
-import React, { Component } from 'react'
-import Link from 'next/link'
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
+import React, { Component } from "react";
+import Link from "next/link";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
 export default class Header extends Component {
   render() {
     return (
       <div className="header-group">
-          <Navbar expand="lg">
-            <div className="header-titles">
-                  <Navbar.Brand href="#home">Beadaut Blogs </Navbar.Brand>
-                  <br />
-                  <p>Curated posts and updates from Beadaut</p>
-            </div>
-                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-                  <Nav>
-                    <Link href={"/posts-list/posts"}>
-                      <a style={{ color:"#FFAD42"}} className="nav-link">Blogs</a>
-                    </Link>
-                    <Link href={"https://www.beadaut.com/"}>
-                      <a style={{ color:"#FFAD42"}} className="nav-link" target="_blank">About</a>
-                    </Link>
-                    <Link href="/">
-                      <a style={{ color:"#FFAD42"}} className="nav-link" target="_blank">Updates</a>
-                    </Link>
-                  </Nav>
-                </Navbar.Collapse>
-          </Navbar>
-        </div>
-    )
+        <Navbar expand="lg">
+          <div className="header-titles">
+            <Navbar.Brand href="/">Beadaut Blogs </Navbar.Brand>
+            <br />
+            <p>Curated posts and updates from Beadaut</p>
+          </div>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse
+            id="basic-navbar-nav"
+            className="justify-content-end"
+          >
+            <Nav>
+              <Link href={"/posts-list/posts"}>
+                <a style={{ color: "#FFAD42" }} className="nav-link">
+                  Blogs
+                </a>
+              </Link>
+              <Link href={"https://www.beadaut.com/about"}>
+                <a
+                  style={{ color: "#FFAD42" }}
+                  className="nav-link"
+                  target="_blank"
+                >
+                  About
+                </a>
+              </Link>
+              <Link href="/">
+                <a
+                  style={{ color: "#FFAD42" }}
+                  className="nav-link"
+                  target="_blank"
+                >
+                  Updates
+                </a>
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    );
   }
 }
