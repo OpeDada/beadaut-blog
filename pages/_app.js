70639f718a892ae2f95a54e601da60f9148e9d86
OpeDada <opedada@gmail.com>
2021-11-03 00:10:27 +0900

    creating a global footer component

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import "../styles/global.scss";
 import Header from "../components/header";
+import Footer from "../components/footer"
 
 export default function App({ Component, pageProps }) {
   return (
     <div>
       <Header />
       <Component {...pageProps} />
+      <Footer />
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import { getSortedPostsData, getAboutData } from "../lib/posts";
 import Link from "next/link";
-import Nav from "react-bootstrap/Nav";
 import Date from "../components/date";
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -121,60 +120,6 @@ export default function Home({ allPostsData, aboutData }) {
                 </Link>
               </div>
             </div>
-          </div>
-        </section>
-        <section className="footer">
-          <div className="footer-top">
-            <div className="footer-links">
-              <Nav>
-                <Link href={"/posts-list/posts"}>
-                  <a className="nav-link">Blogs</a>
-                </Link>
-                <Link href={"https://www.beadaut.com/"}>
-                  <a className="nav-link" target="_blank">
-                    About
-                  </a>
-                </Link>
-                <Link href="/">
-                  <a className="nav-link" target="_blank">
-                    Updates
-                  </a>
-                </Link>
-              </Nav>
-            </div>
-            <div className="footer-socials">
-              <Link href={"https://www.facebook.com/beadautedtech"}>
-                <a className="nav-link" target="_blank">
-                  <FontAwesomeIcon icon={faFacebook} width="24" />
-                </a>
-              </Link>
-              <Link href={"https://twitter.com/beadautedtech"}>
-                <a className="nav-link" target="_blank">
-                  <FontAwesomeIcon icon={faTwitter} width="24" />
-                </a>
-              </Link>
-              <Link href={"https://www.instagram.com/beadautedtech/"}>
-                <a className="nav-link" target="_blank">
-                  <FontAwesomeIcon icon={faInstagram} width="24" />
-                </a>
-              </Link>
-            </div>
-          </div>
-          <div className="footer-recent-posts">
-            <h2>Recent Posts</h2>
-            {allPostsData.map(({ id, title }) => (
-              <li className="" key={id}>
-                <Link href={`/posts/${id}`}>
-                  <a>{title}</a>
-                </Link>
-              </li>
-            ))}
-          </div>
-          <div className="footer-copyright">
-            <p>© 2021 Beadaut Blogs</p>
-            <Link href={"/"}>
-              <span className="">To the top ↑</span>
-            </Link>
           </div>
         </section>
       </Layout>
