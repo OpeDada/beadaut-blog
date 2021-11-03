cb4ef54965fb3e7be0c0de4718cf11f23130027c
Joshua Owoyemi <tjosh.owoyemi@gmail.com>
2021-11-03 12:30:54 +0900

    Remove border bottom in footer,
    apply formating

--- pages/posts-list/posts.js
+++ pages/posts-list/posts.js
@@ -1,15 +1,15 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../../components/layout'
-import utilStyles from '../../styles/utils.module.scss'
-import { getSortedPostsData } from '../../lib/posts'
+import Head from "next/head";
+import Layout, { siteTitle } from "../../components/layout";
+import utilStyles from "../../styles/utils.module.scss";
+import { getSortedPostsData } from "../../lib/posts";
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
+  const allPostsData = getSortedPostsData();
   return {
     props: {
-      allPostsData
-    }
-  }
+      allPostsData,
+    },
+  };
 }
 
 export default function Home({ allPostsData }) {
@@ -33,5 +33,5 @@ export default function Home({ allPostsData }) {
         </ul>
       </section>
     </Layout>
-  )
+  );
 }
