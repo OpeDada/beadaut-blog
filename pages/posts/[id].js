a7f1c1e9927017c89c6a273eacd6e09d0abec4dc
OpeDada <opedada@gmail.com>
2021-11-05 12:10:14 +0900

    individual post page and styling

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,6 @@
 import Head from "next/head";
-import utilStyles from "../../styles/utils.module.scss";
+import Link from "next/link";
+import Image from "next/image";
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import Date from "../../components/date";
@@ -10,12 +11,54 @@ export default function Post({ postData }) {
       <Head>
         <title>{postData.title}</title>
       </Head>
-      <article className="post-page">
-        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-        <div className={utilStyles.lightText}>
-          <Date dateString={postData.date} />
+
+      <article>
+        <div className="article-header">
+          <div className="semi-links">
+            <Link href="#">
+              <a>{postData.category}</a>
+            </Link>
+          </div>
+          <h2>{postData.title}</h2>
+          <p>{postData.intro}</p>
+          <ul className="article-infos">
+            <li style={{ color: "#6d6d6d;" }}>
+              By{" "}
+              <Link href={""}>
+                <a style={{ color: "#6d6d6d;" }}>{postData.author}</a>
+              </Link>
+            </li>
+            <li>
+              <Link href={""}>
+                <a style={{ color: "#6d6d6d;" }}>
+                  <Date dateString={postData.date} />
+                </a>
+              </Link>
+            </li>
+          </ul>
         </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        <Image
+          priority
+          src={postData.image}
+          className=""
+          alt=""
+          layout="responsive"
+          objectFit="cover"
+          width={6}
+          height={4}
+        />
+        <div className="article-content">
+          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+          <div className="article-app-button">
+            <Link href={"https://app.beadaut.com/app/courses"}>
+              <a target="_blank">Start your creator journey on beadaut</a>
+            </Link>
+          </div>
+          <p>
+            Talk soon, <br /> Joshua <br />
+            Founder, Beadaut.
+          </p>
+        </div>
       </article>
     </Layout>
   );
