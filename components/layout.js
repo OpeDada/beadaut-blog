e8346062c77a6963039980bfa6cd4a9d69df64f5
OpeDada <opedada@gmail.com>
2021-11-05 12:07:50 +0900

    removed uneccessary lines of code

--- components/layout.js
+++ components/layout.js
@@ -1,16 +1,11 @@
 import Head from "next/head";
-import Image from "next/image";
-import styles from "./layout.module.scss";
-import utilStyles from "../styles/utils.module.scss";
-import Link from "next/link";
 
 const name = "Beadaut Blogs";
 export const siteTitle = "Beadaut Blogs";
 
-export default function Layout({ children, home }) {
+export default function Layout({ children }) {
   return (
     <div>
-      {/* TODO: please fix the metadata below */}
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta
@@ -21,13 +16,6 @@ export default function Layout({ children, home }) {
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <main>{children}</main>
-      {!home && (
-        <div className={styles.backToHome}>
-          <Link href="/">
-            <a>← Back to home</a>
-          </Link>
-        </div>
-      )}
     </div>
   );
 }
