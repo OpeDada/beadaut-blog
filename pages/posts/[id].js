c407b15d4f2bf69438f2c42f196ed15e9d5c5e14
OpeDada <opedada@gmail.com>
2021-11-09 10:45:44 +0900

    worked on responsiveness

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -6,15 +6,20 @@ import { getAllPostIds, getPostData } from "../../lib/posts";
 import Date from "../../components/date";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTwitter, faFacebook } from "@fortawesome/free-brands-svg-icons";
-import { faThumbsUp } from "@fortawesome/free-solid-svg-icons";
 
 export default function Post({ postData }) {
+  // get all sorted postdata
+  // loop through all postdata
+  // check if id is equal current id
+  // check if there is a post before and after
+  // get post before
+  // get post after
+
   return (
     <Layout>
       <Head>
         <title>{postData.title}</title>
       </Head>
-
       <article>
         <div className="article-header">
           <div className="semi-links">
@@ -42,48 +47,48 @@ export default function Post({ postData }) {
             </li>
           </ul>
         </div>
-        <Image
-          priority
-          src={postData.image}
-          className=""
-          alt=""
-          layout="responsive"
-          objectFit="cover"
-          width={6}
-          height={4}
-        />
-        <div className="article-content">
-          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-          <div className="article-app-button">
-            {postData.cta ? (
-              <Link href={postData.ctaLink}>
-                <a target="_blank">{postData.cta}</a>
-              </Link>
-            ) : null}
+        <div className="other-article-info">
+          <Image
+            priority
+            src={postData.image}
+            className=""
+            alt=""
+            layout="responsive"
+            objectFit="cover"
+            width={6}
+            height={4}
+          />
+          <div className="article-content">
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            <div className="article-app-button">
+              {postData.cta ? (
+                <Link href={postData.ctaLink}>
+                  <a target="_blank">{postData.cta}</a>
+                </Link>
+              ) : null}
+            </div>
           </div>
+          <div className="social-section">
+            <h1>Share this:</h1>
+            <Link href={"https://twitter.com/beadautedtech"}>
+              <a target="_blank" className="social-button">
+                <FontAwesomeIcon icon={faTwitter} width="16" /> Twitter
+              </a>
+            </Link>
+            <Link href={"https://www.facebook.com/beadautedtech"}>
+              <a target="_blank" className="social-button" target="_blank">
+                <FontAwesomeIcon
+                  icon={faFacebook}
+                  width="16"
+                  className="pb-1"
+                />{" "}
+                Facebook
+              </a>
+            </Link>
+          </div>
+          <div className="divider div-transparent div-dot"></div>
+          <div className="divider div-transparent div-dot"></div>
         </div>
-        <div className="social-section">
-          <h1>Share this:</h1>
-          <Link href={"https://twitter.com/beadautedtech"}>
-            <a target="_blank" className="social-button">
-              <FontAwesomeIcon icon={faTwitter} width="16" /> Twitter
-            </a>
-          </Link>
-          <Link href={"https://www.facebook.com/beadautedtech"}>
-            <a target="_blank" className="social-button" target="_blank">
-              <FontAwesomeIcon icon={faFacebook} width="16" className="pb-1" />{" "}
-              Facebook
-            </a>
-          </Link>
-          <Link href={""}>
-            <a target="_blank" className="social-button" target="_blank">
-              <FontAwesomeIcon icon={faThumbsUp} width="16" className="pb-1" />{" "}
-              Like
-            </a>
-          </Link>
-        </div>
-        <div className="divider div-transparent div-dot"></div>
-        <div className="divider div-transparent div-dot"></div>
       </article>
     </Layout>
   );
